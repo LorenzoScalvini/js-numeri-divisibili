a4978cb1028c77ace40a212c2f5dc02e1e84ce4b
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-10-30 23:55:12 +0100

    ADD: Numero divisibile per esercizio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function filtraDivisibili(numero, array) {
+    return array.filter(x => x % numero === 0);
+}
+
+const numeroIntero = 3;
+const arrayDiNumeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const risultato = filtraDivisibili(numeroIntero, arrayDiNumeri);
+
+console.log(risultato); 
